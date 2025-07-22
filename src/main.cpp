2524cf9e3e6a654829e3b54a332e129d9e4e35de
darakof <discordaka10@gmail.com>
2025-07-22 13:59:10 -0500

    improved window handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+
+// screen proportions like the default 16:9
+int proportionsX = 16;
+int proportionsY = 9;
+
+// callback function to make the window's darawable size be the size of the actual window
+void window_size_callback(GLFWwindow* window, int width, int height) {
+    printf("window resized, new size: width %d, height %d\n", width, height);
+    glViewport(0,0,width,height);
+}
 
 int main(void)
 {
@@ -18,12 +30,29 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glClearColor(1.0f,0.0f,1.0f,0.0f);
+    
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        fprintf(stderr,"GLEW failed to initialize, error: %a\n", glewGetErrorString(err));
+    }
+    fprintf(stdout, "using GLEW version %a\n", glewGetString(GLEW_VERSION));
+
+    glfwSetWindowSizeCallback(window, window_size_callback);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
-    {
+    {        
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(1.0f,0.0f,0.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
