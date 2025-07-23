75f51573c433aaba17feb36c7e71d792e889d86f
darakof <discordaka10@gmail.com>
2025-07-22 21:19:00 -0500

    static libraries and compile scripts

---
+++ compile_libs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+cd ./libs/glfw
+mkdir -p build
+cmake -S . -B build -D GLFW_BUILD_X11=1 -D GLFW_BUILD_WAYLAND=1
+cd build
+make
+cd ..
+mkdir -p lib
+cp ./build/src/libglfw3.a ./lib/libglfw3.a
+cp -r ./include ./lib/include
+
+cd ../glew
+make SYSTEM=linux-clang GLEW_DEST=./lib glew.lib.static
+cp -r ./include ./lib/include
+
+echo finished compiling
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <cstring>
+
+// we will read the command line and set this accordingly
+bool DEBUG_MODE = 0;
 
 // callback function to make the window's darawable size be the size of the actual window
 void window_size_callback(GLFWwindow* window, int width, int height) {
-    printf("window resized, new size: width %d, height %d\n", width, height);
+    if (DEBUG_MODE) {
+        printf("window resized, new size: width %d, height %d\n", width, height);
+    }
     glViewport(0,0,width,height);
 }
 
-int main(void)
+// get command line arguments where argc is the ammount and argv is the strings that are the argument (argv[1] is always the executable)
+int main(int argc, char* argv[])
 {
+    for (int i = 1; i < argc; i++) {
+        if (argc < 2) {
+            break;
+        }
+        if (strcmp(argv[i], "-debug") == 0) {
+            DEBUG_MODE = true;
+        }
+    }
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
         return -1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -26,14 +42,29 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    
+    glfwSetWindowSizeCallback(window, window_size_callback);
+
     GLenum err = glewInit();
     if (err != GLEW_OK) {
-        fprintf(stderr,"GLEW failed to initialize, error: %a\n", glewGetErrorString(err));
+        fprintf(stderr, "GLEW intilization return error code %d\n", err);
+        fprintf(stderr,"GLEW failed to initialize, error: %s\n", glewGetErrorString(err));
     }
-    fprintf(stdout, "using GLEW version %a\n", glewGetString(GLEW_VERSION));
+    if (DEBUG_MODE) {
+        printf("using GLEW version %s\n", glewGetString(GLEW_VERSION));
+    }
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
 
-    glfwSetWindowSizeCallback(window, window_size_callback);
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -41,14 +72,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-
-        glColor3f(1.0f,0.0f,0.0f);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
